c38cd545e897793c2b0fcf567f6f41e5ef85a0e2
Steve Young <slyoung@hamilton.edu>
2014-01-09 21:23:10 -0500

    first commit

---
+++ etc/database.sh
@@ -0,0 +1,13 @@
+export DRUPAL_ROOT=$WEB_ROOT/collections	# Top level of the drupal web root
+export DRUPAL_DB_NAME="db_name"          	# Drupal database name
+export DRUPAL_DB_USER="db_user"          	# Drupal username for settings.php file
+export DRUPAL_DB_PASS="password"           	# Drupal password for settings.php file
+export DRUPAL_ADMIN_USER="IslandoraAdmin"       # Drupal admin username to log into Drupal Site
+export DRUPAL_ADMIN_PASS="pass"       		# Drupal admin password to log into Drupal Site
+export DRUPAL_SITE_NAME="Islandora Sandbox"     # Drupal site name - displayed on the web site
+export FEDORA_DB_NAME="fedora3"        		# Name of fedora MySQL database -> fedora3 is recommeneded
+export FEDORA_DB_USER="db_user"        		# Fedora db username -> for fedora.fcfg file
+export FEDORA_DB_PASS="passwordd"         	# Fedora db password -> for fedora.fcfg file
+export FEDORA_ADMIN_USER="fedoraAdmin"          # Username for http://localhost:8080/fedora/admin
+export FEDORA_ADMIN_PASS="password"          	# Password for http://localhost:8080/fedora/admin
+export DB_SERVER=localhost			# hostname of the MySQL server
---
+++ etc/env.sh
@@ -0,0 +1,19 @@
+## Variables
+export FEDORA_HOME=$INSTALL_PREFIX/fedora
+export FEDORA_USER=fedora
+export ISLANDORA_HOME=$INSTALL_PREFIX/islandora
+export ISLANDORA_PKGS=$ISLANDORA_HOME/packages
+export CATALINA_HOME=$FEDORA_HOME/tomcat
+export SOLR_HOME=$INSTALL_PREFIX/solr
+#
+# Java variables
+#
+export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0.x86_64
+export JRE_HOME=$JAVA_HOME/jre
+export JAVA_OPTS="-Xms1024m -Xmx1024m -XX:MaxPermSize=1200m -Djavax.net.ssl.trustStore=$FEDORA_HOME/server/truststore -Djavax.net.ssl.trustStorePassword=tomcat"
+#
+export FEDORA_START=$CATALINA_HOME/bin/startup.sh
+export FEDORA_STOP=$CATALINA_HOME/bin/shutdown.sh
+export PATH=${HOME}/islandora/bin:${HOME}/bin:${PATH}:${JAVA_HOME}/bin
+export LD_LIBRARY_PATH=${HOME}/islandora/lib:${HOME}/fedora/tomcat/lib:${HOME}/lib:${LD_LIBRARY_PATH}
+export LOGPATH=${CATALINA_HOME}/logs
