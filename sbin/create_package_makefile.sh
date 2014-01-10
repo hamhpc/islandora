75bbd95aab524f31686cc8e288e02a575903bf69
Steve Young <slyoung@hamilton.edu>
2014-01-10 15:58:57 -0500

    added create_package_makefile.sh script for configuring package Makefiles

---
+++ sbin/create_package_makefile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+INSTALL_CMD="install:"
+UNINSTALL_CMD="uninstall:"
+
+for DIR in bin etc include info lib man sbin share
+do
+	for DIR_CONTENTS in `ls`
+	do
+		if [ $DIR_CONTENTS == $DIR ]; then
+			INSTALL_CMD="$INSTALL_CMD install-$DIR"
+			UNINSTALL_CMD="$UNINSTALL_CMD uninstall-$DIR"
+		fi	
+	done
+done
+echo $INSTALL_CMD
+echo $UNINSTALL_CMD
+
+cp ../Makefile .
+
+sed -i "s/INSTALL_PACKAGE_COMMAND/$INSTALL_CMD/" ./Makefile
+sed -i "s/UNINSTALL_PACKAGE_COMMAND/$UNINSTALL_CMD/" ./Makefile
