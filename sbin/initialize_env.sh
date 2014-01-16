c2d428352e39f45cafe485ff43a88c5696b22781
hamhpc <slyoung@hamilton.edu>
2014-01-16 18:25:46 -0500

    updated sed commands with |
    since we're passing path's in sed the / in the path's are messing with the sed command so we need to change the sed delimiter from / to | so it doesn't get confused with the paths in the variables.

--- sbin/initialize_env.sh
+++ sbin/initialize_env.sh
@@ -8,14 +8,14 @@
 #---------------------------------------------
 #
 
-sed -i "s/FEDORA_HOME_TO_CHANGE/$FEDORA_HOME/g" ../etc/env.sh
+sed -i "s|FEDORA_HOME_TO_CHANGE|$FEDORA_HOME|g" ../etc/env.sh
 sed -i "s/FEDORA_USER_TO_CHANGE/$FEDORA_USER/g" ../etc/env.sh
-sed -i "s/ISLANDORA_HOME_TO_CHANGE/$ISLANDORA_HOME/g" ../etc/env.sh
-sed -i "s/SOLR_HOME_TO_CHANGE/$SOLR_HOME/g" ../etc/env.sh
-sed -i "s/JAVA_HOME_TO_CHANGE/$JAVA_HOME/g" ../etc/env.sh
+sed -i "s|ISLANDORA_HOME_TO_CHANGE|$ISLANDORA_HOME|g" ../etc/env.sh
+sed -i "s|SOLR_HOME_TO_CHANGE|$SOLR_HOME|g" ../etc/env.sh
+sed -i "s|JAVA_HOME_TO_CHANGE|$JAVA_HOME|g" ../etc/env.sh
 
 sed -i "s/DATABASE_SERVER_TO_CHANGE/$DB_SERVER/g" ../etc/database.sh
-sed -i "s/DRUPAL_ROOT_TO_CHANGE/$DRUPAL_ROOT/g" ../etc/database.sh
+sed -i "s|DRUPAL_ROOT_TO_CHANGE|$DRUPAL_ROOT|g" ../etc/database.sh
 sed -i "s/DRUPAL_DB_NAME_TO_CHANGE/$DRUPAL_DB_NAME/g" ../etc/database.sh
 sed -i "s/DRUPAL_DB_USER_TO_CHANGE/$DRUPAL_DB_USER/g" ../etc/database.sh
 sed -i "s/DRUPAL_DB_PASS_TO_CHANGE/$DRUPAL_DB_PASS/g" ../etc/database.sh
