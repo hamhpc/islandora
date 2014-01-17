15cf91aaf6b4b8b2cba9aaa295368bb556cfc3fe
Steve Young <slyoung@hamilton.edu>
2014-01-16 21:44:33 -0500

    updates

--- sbin/set_permissions.sh
+++ sbin/set_permissions.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
+#
+# Script to set permissions
+#
+# 1/9/2014 SLY
+
+#
+#
+#
+# Source environment
+. ../etc/env.sh
+. ../etc/database.sh
+
 
 #
 ## Set the permissions on the whole installation
@@ -7,6 +18,8 @@
 # set ownership
 chown -R ${FEDORA_USER}:${FEDORA_USER} ${ISLANDORA_HOME}
 chown -R ${FEDORA_USER}:${FEDORA_USER} ${FEDORA_HOME}
+chown -R ${FEDORA_USER}:${FEDORA_USER} ${DRUPAL_ROOT}
 
 # set permissions
 chmod -R 755 ${ISLANDORA_HOME} ${FEDORA_HOME}
+chmod -R 640 ${ISLANDORA_HOME}/etc ${FEDORA_HOME}
