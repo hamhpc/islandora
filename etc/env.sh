50419f6f2b6c4d939c3c747aee1bf1d851500060
Steve Young <slyoung@hamilton.edu>
2014-01-27 13:03:08 -0500

    Update env.sh

--- etc/env.sh
+++ etc/env.sh
@@ -13,6 +13,10 @@ export CATALINA_HOME=$FEDORA_HOME/tomcat
 export SOLR_HOME=SOLR_HOME_TO_CHANGE
 export FEDORA_START=$CATALINA_HOME/bin/startup.sh
 export FEDORA_STOP=$CATALINA_HOME/bin/shutdown.sh
+export DJATOKA_HOME=$ISLANDORA_PKGS/adore-djatoka-1.1
+export KAKADU_HOME=$DJATOKA_HOME/bin/Linux-x86-64
+export KAKADU_LIBRARY_PATH="-DLD_LIBRARY_PATH=$DJATOKA_HOME/lib/Linux-x86-64"
+
 #---------------------------------------------
 #
 # JAVA environment variables 
@@ -22,6 +26,8 @@ export FEDORA_STOP=$CATALINA_HOME/bin/shutdown.sh
 export JAVA_HOME=JAVA_HOME_TO_CHANGE
 export JRE_HOME=$JAVA_HOME/jre
 export JAVA_OPTS="-Xms1024m -Xmx1024m -XX:MaxPermSize=1200m -Djavax.net.ssl.trustStore=$FEDORA_HOME/server/truststore -Djavax.net.ssl.trustStorePassword=tomcat"
+# for djatoka
+export JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true -Dkakadu.home=$KAKADU_HOME -Djava.library.path=$DJATOKA_HOME/lib/Linux-x86-64 $KAKADU_LIBRARY_PATH"
 #---------------------------------------------
 #
 # User environment variables 
@@ -29,5 +35,5 @@ export JAVA_OPTS="-Xms1024m -Xmx1024m -XX:MaxPermSize=1200m -Djavax.net.ssl.trus
 #---------------------------------------------
 #
 export PATH=$ISLANDORA_HOME/bin:${ISLANDORA_HOME}/sbin:${HOME}/bin:${JAVA_HOME}/bin:${PATH}
-export LD_LIBRARY_PATH=$ISLANDORA_HOME/lib:$CATALINA_HOME/lib:${HOME}/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=$ISLANDORA_HOME/lib:$CATALINA_HOME/lib:${HOME}/lib:$DJATOKA_HOME/lib:$DJATOKA_HOME/lib/Linux-x86-64:${LD_LIBRARY_PATH}
 export LOGPATH=${CATALINA_HOME}/logs
